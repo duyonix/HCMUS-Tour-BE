a417eadcbc35b3b3fc0909d813757c6f37d1db2e
duyonix <dtduy2210@gmail.com>
2023-03-05 17:39:08 +0700

    initial

---
+++ src/main/java/com/onix/hcmustour/HCMUSTourApplication.java
@@ -0,0 +1,13 @@
+package com.onix.hcmustour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HCMUSTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HCMUSTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onix/hcmustour/HcmusTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onix.hcmustour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcmusTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
